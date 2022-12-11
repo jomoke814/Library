aaf64025c8f167bc2d15a79430cbfc6951897185
jomoke814 <jcullen814@gmail.com>
2022-12-11 12:21:25 +0300

    Refactoring library to use class instead of plan constructors

--- js/Library.js
+++ js/Library.js
@@ -4,96 +4,110 @@ const clearButton = document.querySelector('.reset-btn')
 
 let myLibrary = [];
 
-//Constructor
-function Book (Title, Author, Pages, Read) {
-    this.Title =  Title;
-    this.Author = Author;
-    this.Pages = Pages;
-    this.Read = Read;
+//Class
+class Book {
+    constructor(Title, Author, Pages, Read) {
+        this.Title =  Title;
+        this.Author = Author;
+        this.Pages = Pages;
+        this.Read = Read;
+    }
 }
 
 // Adding a book function
-function addBookToLibrary (Title, Author, Pages, Read) {
-    let book = new Book(Title, Author, Pages, Read)
-        myLibrary.push(book);
-        displayBooks();
-}
+// const addBookToLibrary = (Title, Author, Pages, Read) {
+//     let book = new Book(Title, Author, Pages, Read)
+//         myLibrary.push(book);
+//         displayBooks();
+// }
 
 // function to display books
-function displayBooks() {
-    const books = document.querySelector(".books-container");
-    const removeDivs = document.querySelectorAll('.card');
+class UI {
+   
 
-    for(let i = 0; i < removeDivs.length; i++) {
-        removeDivs[i].remove();
+    static displayBooks() {
+        const books = document.querySelector(".books-container");
+        const removeDivs = document.querySelectorAll('.card');
+    
+        for(let i = 0; i < removeDivs.length; i++) {
+            removeDivs[i].remove();
+        }
+    
+        let index = 0
+        myLibrary.forEach(myLibrarys => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            books.appendChild(card);
+    
+            //looping through value to display card
+            for(let key in myLibrarys) {
+                const para = document.createElement('p');
+                para.textContent = (`${key}: ${myLibrarys[key]}`);
+                card.appendChild(para);
+            }
+    
+            //create remove button
+            const removeButton = document.createElement('button');
+            removeButton.classList.add('delete-btn');
+            removeButton.textContent = "Delete"
+    
+            //link remove button with array
+            removeButton.dataset.linkedArray = index;
+            card.appendChild(removeButton);
+    
+            //eventlistener for remove button to delete card and remove data from array
+            removeButton.addEventListener("click", ()=> {
+                let retrieveBook = removeButton.dataset.linkedArray;
+                myLibrary.splice(parseInt(retrieveBook), 1);
+                card.remove();
+                displayBooks();
+            })
+    
+            //creating button to change read status
+            const readButton = document.createElement('Button');
+            readButton.classList.add('read-btn');
+            readButton.textContent = 'Toggle status';
+    
+            //link read button with array data
+            readButton.dataset.linkedArray = index;
+            card.appendChild(readButton);
+    
+            //create eventListener to toggle read status button
+            readButton.addEventListener('click', () => {
+                let retrieveBookToggle = readButton.dataset.linkedArray;
+                Book.prototype = Object.create(Book.prototype);
+                const toggleBook = new Book();
+    
+                //checking read status
+                if ((myLibrary[parseInt(retrieveBookToggle)].Read) == 'Yes') {
+                    toggleBook.Read =  'No';
+                    myLibrary[parseInt(retrieveBookToggle)].Read = toggleBook.Read; 
+                } else if ((myLibrary[parseInt(retrieveBookToggle)].Read) == 'No') {
+                    toggleBook.Read = 'Yes';
+                    myLibrary[parseInt(retrieveBookToggle)].Read = toggleBook.Read;
+                }
+                displayBooks();
+            })
+    
+    
+        index ++;
+        })
     }
 
-    let index = 0
-    myLibrary.forEach(myLibrarys => {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        books.appendChild(card);
-
-        //looping through value to display card
-        for(let key in myLibrarys) {
-            const para = document.createElement('p');
-            para.textContent = (`${key}: ${myLibrarys[key]}`);
-            card.appendChild(para);
-        }
-
-        //create remove button
-        const removeButton = document.createElement('button');
-        removeButton.classList.add('delete-btn');
-        removeButton.textContent = "Delete"
-
-        //link remove button with array
-        removeButton.dataset.linkedArray = index;
-        card.appendChild(removeButton);
-
-        //eventlistener for remove button to delete card and remove data from array
-        removeButton.addEventListener("click", ()=> {
-            let retrieveBook = removeButton.dataset.linkedArray;
-            myLibrary.splice(parseInt(retrieveBook), 1);
-            card.remove();
-            displayBooks();
-        })
-
-        //creating button to change read status
-        const readButton = document.createElement('Button');
-        readButton.classList.add('read-btn');
-        readButton.textContent = 'Toggle status';
-
-        //link read button with array data
-        readButton.dataset.linkedArray = index;
-        card.appendChild(readButton);
-
-        //create eventListener to toggle read status button
-        readButton.addEventListener('click', () => {
-            let retrieveBookToggle = readButton.dataset.linkedArray;
-            Book.prototype = Object.create(Book.prototype);
-            const toggleBook = new Book();
-
-            //checking read status
-            if ((myLibrary[parseInt(retrieveBookToggle)].Read) == 'Yes') {
-                toggleBook.Read =  'No';
-                myLibrary[parseInt(retrieveBookToggle)].Read = toggleBook.Read; 
-            } else if ((myLibrary[parseInt(retrieveBookToggle)].Read) == 'No') {
-                toggleBook.Read = 'Yes';
-                myLibrary[parseInt(retrieveBookToggle)].Read = toggleBook.Read;
-            }
-            displayBooks();
-        })
-
-
-    index ++;
-    })
+    static addBookToLibrary (Title, Author, Pages, Read) {
+        let book = new Book(Title, Author, Pages, Read)
+            myLibrary.push(book);
+            UI.displayBooks();
+    }
 }
 
 addBookButton.addEventListener("click", () => {
     document.getElementById("add-book-form").style.display = "block";
 })
 
-submitButton.addEventListener("click", () => {
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
     let Title = document.getElementById('Title').value;
     let Author = document.getElementById('Author').value;
     let Pages = document.getElementById('Pages').value;
@@ -103,7 +117,7 @@ submitButton.addEventListener("click", () => {
         return;
     }
 
-    addBookToLibrary(Title, Author, Pages, Read);
+    UI.addBookToLibrary(Title, Author, Pages, Read);
 
     document.getElementById('book-form').reset();
 })
